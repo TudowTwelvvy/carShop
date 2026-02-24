f21630a77b66cc348832b83f3eca373d852a925a
TudowTwelvvy <tumelokhanye9811@gmail.com>
2026-02-24 14:31:54 +0200

    Add project files.

---
+++ carShop/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace carShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        //GET: /home/about/{id}
+        public ActionResult About(string id)
+        {
+            ViewBag.Message = "Your application description page." + id;
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ carShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="carShop.MvcApplication" Language="C#" %>
