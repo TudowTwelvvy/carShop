5cad4aa67e5b0ac1ccccf0bbfc0eed66591ef301
TudowTwelvvy <tumelokhanye9811@gmail.com>
2026-02-25 09:52:22 +0200

    created the Car and Category models

---
+++ carShop/Models/Car.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace carShop.Models
+{
+    public class Car
+    {
+        public int ID { get; set; }  //USED FOR PRIMARY KEY-CODE
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int? CategoryID { get; set; } //Represents the ID of the category that the product is assigned to: FOREIGN KEY-CODE
+        public virtual Category Category { get; set; }
+
+    }
+}
---
+++ carShop/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace carShop.Models
+{
+    public class Category
+    {
+        public int ID { get; set; } //PRIMARY KEY-CODE
+        public string Name { get; set; }
+        public virtual ICollection<Car> Cars { get; set; }
+    }
+}
