3a6e04da440893d029985a2f274b3101d5046113
TudowTwelvvy <tumelokhanye9811@gmail.com>
2026-02-25 10:32:19 +0200

    connected the EF to the database

---
+++ carShop/DAL/ShopContext.cs
@@ -0,0 +1,15 @@
+﻿using carShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace carShop.DAL
+{
+    public class ShopContext
+    {
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
