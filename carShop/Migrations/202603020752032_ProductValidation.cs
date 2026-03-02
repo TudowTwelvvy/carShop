6af7bd244f10e9ea9866a94b68563edf1951dace
TudowTwelvvy <tumelokhanye9811@gmail.com>
2026-03-02 10:13:06 +0200

    added formatting and validation to the product class

---
+++ carShop/Migrations/202603020752032_ProductValidation.cs
@@ -0,0 +1,20 @@
+﻿namespace carShop.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class ProductValidation : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Car", "Name", c => c.String(nullable: false, maxLength: 50));
+            AlterColumn("dbo.Car", "Description", c => c.String(nullable: false, maxLength: 200));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Car", "Description", c => c.String());
+            AlterColumn("dbo.Car", "Name", c => c.String());
+        }
+    }
+}
--- carShop/Models/Car.cs
+++ carShop/Models/Car.cs
@@ -9,8 +9,26 @@ namespace carShop.Models
     public partial class Car
     {
         public int ID { get; set; }  //USED FOR PRIMARY KEY-CODE
+
+        [Required(ErrorMessage = "Please enter the name of the product.")]
+        [StringLength(50, MinimumLength = 3, 
+            ErrorMessage = "Please enter a product name between 3 and 50 characters in length.")] 
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description of the product.")]
+        [StringLength(200, MinimumLength = 10, 
+            ErrorMessage = "Please enter a product description between 10 and 200 characters in length.")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$",
+            ErrorMessage = "Please enter a product name beginning with a capital letter and made up of letters and spaces.")]
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Please enter the price of the product.")]
+        [Range(0.01, 1000000, ErrorMessage = "Please enter a price between 0.01 and 1,000,000.")]
+        [DataType(DataType.Currency)]
+        [DisplayFormat(DataFormatString = "{0:C}")]
+        [RegularExpression("[0-9]+(\\.[0-9][0-9]?)?", 
+            ErrorMessage = "The price must be a number up to two decimals")]
         public decimal Price { get; set; }
         public int? CategoryID { get; set; } //Represents the ID of the category that the product is assigned to: FOREIGN KEY-CODE
         public virtual Category Category { get; set; }
